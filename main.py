6c65b1f1dd098fce6bbb230e468096b831ed19e3
Chanchal Hiwanj <chanchalhiwanj@gmail.com>
2023-09-14 20:02:11 +0530

    new changes

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # import pyfiglet
-
 import os
 import requests
 # Add your API keys here
-SHODAN_API_KEY = "YOUR_SHODAN_API_KEY"
+SHODAN_API_KEY = "bm2DmZaQIEw5oXcOVx4qy3HPXF5PrjZi"
 ONYPHE_API_KEY = "YOUR_ONYPHE_API_KEY"
 WIGLE_API_KEY = "YOUR_WIGLE_API_KEY"
 GOOGLE_API_KEY = "YOUR_GOOGLE_API_KEY"
@@ -16,20 +15,6 @@ BINARYEDGE_API_KEY = "YOUR_BINARYEDGE_API_KEY"
 FULLHUNT_API_KEY = "YOUR_FULLHUNT_API_KEY"
 NELAS_API_KEY = "YOUR_NELAS_API_KEY"
 # Add other API keys here as needed
-def Mainpage():
-    print('''
-    [1] Information Gathering
-    [2] Scanning
-    [3] Password Attacks
-    [4] Exploitation
-    [5] Post-Exploitation
-    [6] Sniffing and Spoofing
-    [7] Web Hacking
-    [8] Report
-    [9] Install/ Update
-    [0] Exit
-    ''')
-    print("[+]choose:")
 
 def Mylogo():
     print('''\033[1;32;40m                                                         ██
@@ -44,12 +29,59 @@ def Mylogo():
     print("\n   \\\---------------Scripted by Arpit, Chanchal, Chirayu, Pranjal---------------//")
 
 
-if __name__ == "__main__":
-    Mylogo()
-    Mainpage()
+# home page menu
+def Mainpage():
+    print('''
+    [1] Information Gathering
+    [2] Scanning
+    [3] Password Attacks
+    [4] Exploitation
+    [5] Post-Exploitation
+    [6] Sniffing and Spoofing
+    [7] Web Hacking
+    [8] Report
+    [9] Install/ Update
+    [0] Exit
+    ''')
+    choice=input("[+]Choose:")
+
+    if choice == "1":
+        os.system('clear')
+        main()
+    elif choice == "2":
+        print("Scanning Phase under construction")
+    elif choice == "2":
+        print("password attacks")
+    elif choice == "2":
+        print("Exploitation")
+    elif choice == "2":
+        print("post Exploitation")
+    elif choice == "2":
+        print("sniffing and spoofing")
+    elif choice == "2":
+        print("web hacking")
+    elif choice == "2":
+        print("report")
+    elif choice == "2":
+        print("install/ update")
+    elif choice == "0":
+        print("Exiting...")
+        return
+    else:
+        print("Invalid choice. Please select a valid option.")
+
+
+
+
+
+
+# functions starts here------------------------------
+
+  ##infomration gathering
 
 def shodan_search():
     print("You selected Shodan")
+
     ip_address = input("Enter the IP address you want to search: ")
     response = requests.get(f"https://api.shodan.io/shodan/host/{ip_address}", headers={"Authorization": f"Bearer {SHODAN_API_KEY}"})
     if response.status_code == 200:
@@ -79,7 +111,6 @@ def ivre_search():
         print("Error connecting to Ivre API.")
 
 def main():
-    print("Marauders")
     while True:
         print("Select an option:")
         print("1. Server")
@@ -121,3 +152,7 @@ def main():
 
 
 
+
+if __name__ == "__main__":
+    Mylogo()
+    Mainpage()
